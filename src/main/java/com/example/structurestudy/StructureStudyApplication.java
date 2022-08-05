de3b5afe5b24d6b488fb64a5a8ec3933c313248d
SMJ <smj@SMJs-MacBook-Pro.local>
2022-08-05 11:03:22 +0900

    structure Study Setting

---
+++ src/main/java/com/example/structurestudy/StructureStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.structurestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructureStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructureStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/structurestudy/StructureStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.structurestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructureStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
